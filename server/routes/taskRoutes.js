7522fc8745367aff4bdfe6edbf40791eaf47a758
Edun Omatseye <edunomatseye@yahoo.com>
2024-12-17 20:18:01 +0100

    feat(core) add getTask controller to router

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import * as taskController from '../controllers/taskController.js';
+import express from "express";
+import * as taskController from "../controllers/taskController.js";
 
 const router = express.Router();
 
-router.get('/items', taskController.getAllTasks);
-router.post('/items', taskController.createTask);
-router.delete('/items/:id', taskController.deleteTask);
+router.get("/items", taskController.getAllTasks);
+router.post("/items", taskController.createTask);
+router.get("/items/:id", taskController.getTask);
+router.delete("/items/:id", taskController.deleteTask);
 
 export default router;
